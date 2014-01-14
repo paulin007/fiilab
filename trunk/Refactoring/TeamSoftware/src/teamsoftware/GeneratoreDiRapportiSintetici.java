0470a92130ff8c5ba7e33a0c5f3647ab7ee0dc4c
alemar05@unipv.it <alemar05@unipv.it@33fbb97a-6e05-434b-bdf8-5ffe32220524>
2014-01-14 06:41:02 +0000

    Ulteriore estrazione di Metodi; sono estratti i metodi che corrispondono alle funzionalità di conversione di Stringhe da Log e viceversa.

--- trunk/Refactoring/TeamSoftware/src/teamsoftware/GeneratoreDiRapportiSintetici.java
+++ trunk/Refactoring/TeamSoftware/src/teamsoftware/GeneratoreDiRapportiSintetici.java
@@ -13,53 +13,71 @@ public class GeneratoreDiRapportiSintetici {
 		for (String rapporto : rapportoCompleto) {
 			
 			//"Giuseppe Verdi 4 Testing 01-03-2014",
-			StringTokenizer tokenizer=new StringTokenizer(rapporto);
-		
-			String nome=tokenizer.nextToken();
-			String cognome=tokenizer.nextToken();
-			String ore=tokenizer.nextToken();
-			String compito=tokenizer.nextToken();
-			String data=tokenizer.nextToken();
+			Log log = creaLogDaRapportoCompleto(rapporto);
 			
-			Log log=new Log(nome, cognome, ore, compito);
-			
-			boolean trovato = false;
-			trovato = cercaRisultato(risultato, log,
-					trovato);
+			boolean trovato = cercaRisultato(risultato, log);
 			
 			if(!trovato){
-				risultato.add(nome+" "+cognome+" "+ore+" "+compito+" "+ore);
+				risultato.add(creaRapportoSinteticoDaLog(log));
 			}
 		}
 		
 		return risultato;
 	}
 
-	private boolean cercaRisultato(ArrayList<String> risultato,
-			Log log, boolean trovato) {
+
+	private Log creaLogDaRapportoCompleto(String rapporto) {
+		StringTokenizer tokenizer=new StringTokenizer(rapporto);
+
+		String nome=tokenizer.nextToken();
+		String cognome=tokenizer.nextToken();
+		String ore=tokenizer.nextToken();
+		String compito=tokenizer.nextToken();
+		String data=tokenizer.nextToken();
+		
+		Log log=new Log(nome, cognome, ore, compito);
+		return log;
+	}
+
+	private String creaRapportoSinteticoDaLog(Log log) {
+		return log.getNome()+" "+log.getCognome()+" "+
+			log.getOre()+" "+log.getCompito()+" "+log.getOre();
+	}
+	
+	private boolean cercaRisultato(ArrayList<String> risultato,Log log) {
+		boolean trovato = false;
 		for (int i = 0; i < risultato.size(); i++) {
 			
-			StringTokenizer tokenizer2=new StringTokenizer(risultato.get(i));
-			//"Giuseppe Verdi 12 Testing 12",
+			String string=risultato.get(i);
+			Log log2 = creaLogDaRapportoSintetico(string);
 			
-			String nome2=tokenizer2.nextToken();
-			String cognome2=tokenizer2.nextToken();
-			String ore2=tokenizer2.nextToken();
-			String compito2=tokenizer2.nextToken();
-			
-			if(log.getNome().equalsIgnoreCase(nome2)){
-				if(log.getCognome().equalsIgnoreCase(cognome2)){
-					if(log.getCompito().equalsIgnoreCase(compito2)){
+			if(log.getNome().equalsIgnoreCase(log2.getNome())){
+				if(log.getCognome().equalsIgnoreCase(log2.getCognome())){
+					if(log.getCompito().equalsIgnoreCase(log2.getCompito())){
 						trovato=true;
 						risultato.remove(i);
-						int sommaOre=Integer.parseInt(log.getOre())+Integer.parseInt(ore2);
-						risultato.add(log.getNome()+" "+log.getCognome()+" "+
-								sommaOre+" "+log.getCompito()+" "+sommaOre);
+						int sommaOre=Integer.parseInt(log.getOre())+
+							Integer.parseInt(log2.getOre());
+						log2.setOre(""+sommaOre);
+						risultato.add(creaRapportoSinteticoDaLog(log2));
 						i=risultato.size();
 					}
 				}
 			}
 		}
 		return trovato;
+	}
+
+	private Log creaLogDaRapportoSintetico(String string) {
+		StringTokenizer tokenizer2=new StringTokenizer(string);
+		//"Giuseppe Verdi 12 Testing 12",
+		
+		String nome2=tokenizer2.nextToken();
+		String cognome2=tokenizer2.nextToken();
+		String ore2=tokenizer2.nextToken();
+		String compito2=tokenizer2.nextToken();
+		
+		Log log2=new Log(nome2, cognome2, ore2, compito2);
+		return log2;
 	}
 }
