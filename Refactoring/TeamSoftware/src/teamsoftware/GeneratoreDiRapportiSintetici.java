6e92c20022c43ad41378d65220a8a9dd3898eac8
alemar05@unipv.it <alemar05@unipv.it@33fbb97a-6e05-434b-bdf8-5ffe32220524>
2014-01-14 06:29:55 +0000

    Sharing Project 'Refactoring/TeamSoftware'

---
+++ Refactoring/TeamSoftware/src/teamsoftware/GeneratoreDiRapportiSintetici.java
@@ -0,0 +1,14 @@
+package teamsoftware;
+
+import java.util.ArrayList;
+
+public class GeneratoreDiRapportiSintetici {
+
+	public ArrayList<String> generaRapportoSintetico(
+			ArrayList<String> rapportoCompleto){
+		
+		//TODO
+		return new ArrayList<String>();
+	}
+
+}
