3fd8f0a766fc30013589767aab6d67dca6132d76
alemar05@unipv.it <alemar05@unipv.it@33fbb97a-6e05-434b-bdf8-5ffe32220524>
2014-03-11 15:48:24 +0000

    Verifica280214: Generata Interfaccia Grafica

--- Verifica280214/src/scarpe/Scarpe.java
+++ Verifica280214/src/scarpe/Scarpe.java
@@ -1,7 +1,14 @@
 package scarpe;
 
+import java.awt.GridLayout;
+import java.awt.TextField;
 import java.io.File;
 import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Scarpe {
 
@@ -18,6 +25,40 @@ public class Scarpe {
 			e.printStackTrace();
 		}
 		
+		JPanel fieldsPanel=new JPanel();
+			
+			fieldsPanel.setLayout(new GridLayout(3, 1));
+			
+			TextField fieldNumber=new TextField();
+			fieldsPanel.add(fieldNumber);
+
+			TextField fieldSize=new TextField();
+			fieldsPanel.add(fieldSize);
+
+			TextField fieldModel=new TextField();
+			fieldsPanel.add(fieldModel);
 		
+		JPanel resultsPanel=new JPanel();
+		
+			resultsPanel.setLayout(new GridLayout(2, 1));
+			
+			JLabel resultLabel=new JLabel();
+			
+			JButton insertButton=new JButton("Insert");
+			
+			resultsPanel.add(insertButton);
+			resultsPanel.add(resultLabel);
+			
+		
+		JFrame frame=new JFrame();
+		frame.setTitle("Stocks");
+		frame.setSize(150, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+			frame.getContentPane().setLayout(new GridLayout(2, 1));
+			frame.getContentPane().add(fieldsPanel);
+			frame.getContentPane().add(resultsPanel);
+			
+		frame.setVisible(true);
 	}
 }
