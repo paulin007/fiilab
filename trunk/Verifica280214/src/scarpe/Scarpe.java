2cd8cb48953b4072efc62994bc98cd0225842f7d
alemar05@unipv.it <alemar05@unipv.it@33fbb97a-6e05-434b-bdf8-5ffe32220524>
2014-03-11 15:28:50 +0000

    Verifica280214: Risolto Punto (1)

--- trunk/Verifica280214/src/scarpe/Scarpe.java
+++ trunk/Verifica280214/src/scarpe/Scarpe.java
@@ -1,5 +1,23 @@
 package scarpe;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Scarpe {
 
+	public static final String FILENAME = "stock.txt";
+	
+	public static void main(String[] args) {
+
+		try {
+			File file=new File(FILENAME);
+			if(!file.exists()){
+				file.createNewFile();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
 }
