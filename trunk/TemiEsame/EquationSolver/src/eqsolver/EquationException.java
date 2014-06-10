9af5058a8efb2f1d24209d43be9c5ff28c60e7ac
alemar05@unipv.it <alemar05@unipv.it@33fbb97a-6e05-434b-bdf8-5ffe32220524>
2014-06-10 08:29:11 +0000

    Sharing Exame 'EquationEditor'

---
+++ trunk/TemiEsame/EquationSolver/src/eqsolver/EquationException.java
@@ -0,0 +1,11 @@
+package eqsolver;
+
+public class EquationException extends Exception{
+
+	private static final long serialVersionUID=0;
+	
+	public EquationException(String message) {
+		super(message);
+	}
+	
+}
