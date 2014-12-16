63d94e23859641dc7bec5d4a9a8f893f4d51ead4
alemar05@unipv.it <alemar05@unipv.it@33fbb97a-6e05-434b-bdf8-5ffe32220524>
2014-12-16 15:33:09 +0000

    Test01 funzionante

--- Refactoring/TeamSoftware2/src/teamsoftware/GeneratoreDiRapportiSintetici.java
+++ Refactoring/TeamSoftware2/src/teamsoftware/GeneratoreDiRapportiSintetici.java
@@ -1,14 +1,58 @@
 package teamsoftware;
 
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class GeneratoreDiRapportiSintetici {
 
 	public ArrayList<String> generaRapportSintetico(
 			ArrayList<String> rapportoCompleto){
 		
+		ArrayList<String> risultato=new ArrayList<String>();
 		
-		return new ArrayList<String>();
+		for (String rapporto : rapportoCompleto) {
+			
+			StringTokenizer tokenizer=new StringTokenizer(rapporto);
+			
+			String nome=tokenizer.nextToken();
+			String cognome=tokenizer.nextToken();
+			String ore=tokenizer.nextToken();
+			String compito=tokenizer.nextToken();
+			String data=tokenizer.nextToken();
+			
+			boolean trovato = false;
+			
+			for (int i = 0; i < risultato.size(); i++) {
+				
+				StringTokenizer tokenizer2 =new StringTokenizer(risultato.get(i));
+
+				String nome2=tokenizer2.nextToken();
+				String cognome2=tokenizer2.nextToken();
+				String ore2=tokenizer2.nextToken();
+				String compito2=tokenizer2.nextToken();
+				
+				if(nome.equalsIgnoreCase(nome2)){
+					if(cognome.equalsIgnoreCase(cognome2)){
+						if(compito.equalsIgnoreCase(compito2)){
+							trovato = true;
+							risultato.remove(i);
+							int sommaOre = Integer.parseInt(ore)+
+								Integer.parseInt(ore2);
+							risultato.add(nome+" "+cognome+" "+
+									sommaOre+" "+compito+" "+sommaOre);
+							i=risultato.size();
+						}
+					}
+				}
+			}
+			
+			if(!trovato){
+				risultato.add(nome+" "+cognome+" "+
+						ore+" "+compito+" "+ore);
+			}
+		}
+		
+		return risultato;
 	}
 	
 }
