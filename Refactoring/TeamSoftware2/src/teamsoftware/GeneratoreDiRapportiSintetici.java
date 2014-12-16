aaf4821ba728e87cea8254484cf8a8f66ef6312e
alemar05@unipv.it <alemar05@unipv.it@33fbb97a-6e05-434b-bdf8-5ffe32220524>
2014-12-16 15:11:19 +0000

    Primo Test Pronto

---
+++ Refactoring/TeamSoftware2/src/teamsoftware/GeneratoreDiRapportiSintetici.java
@@ -0,0 +1,14 @@
+package teamsoftware;
+
+import java.util.ArrayList;
+
+public class GeneratoreDiRapportiSintetici {
+
+	public ArrayList<String> generaRapportSintetico(
+			ArrayList<String> rapportoCompleto){
+		
+		
+		return new ArrayList<String>();
+	}
+	
+}
